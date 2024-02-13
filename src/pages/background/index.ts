a68413e689175e5ab0c3be471d070599a62398ed
kennethcamacho <46813058+kentuy17@users.noreply.github.com>
2024-02-13 20:02:46 +0800

    oten-source2

--- src/pages/background/index.ts
+++ src/pages/background/index.ts
@@ -9,4 +9,32 @@ reloadOnUpdate('pages/background');
  */
 reloadOnUpdate('pages/content/style.scss');
 
-console.log('background loaded');
+console.log('background loadeds');
+
+const KEYWORDS = ['youtube', 'anime', 'manga', 'aniwave'];
+
+const DUMMY_FCK = [
+  'nTrac Ubuntu Private Server',
+  'Chat | Tracking System | Microsoft Teams',
+  'GitHub — Firefox Developer Edition',
+  'MySQL Workbench',
+  '*new 4 - Notepad++ [Administrator]',
+  'Tickets - Katalyst Software Services @ katalyst.raiseaticket.com and 1 more page - Work - Microsoft​ Edge',
+  'Newest reactjs Questions - Stack Overflow',
+  'Introduction - shadcn/ui',
+];
+
+chrome.tabs.onActivated.addListener(tab => {
+  chrome.tabs.get(tab.tabId, tab => {
+    const urlName = '' + tab.url;
+    const initUrl = '' + tab.pendingUrl;
+
+    KEYWORDS.forEach(keyword => {
+      if (urlName.includes(keyword) || initUrl.includes(keyword)) {
+        const title = DUMMY_FCK[Math.floor(Math.random() * DUMMY_FCK.length)];
+        // eslint-disable-next-line @typescript-eslint/no-unused-vars
+        chrome.tabs.sendMessage(tab.id, { title: title }, function (response) {});
+      }
+    });
+  });
+});
